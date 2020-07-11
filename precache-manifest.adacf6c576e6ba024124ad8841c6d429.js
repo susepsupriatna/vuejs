327e08074dcb9039b948d7bf23a9c1cc52c3cd03
Susep Supriatna <susepsupriatna@gmail.com>
2020-07-11 18:49:31 +0700

    Add files via upload

---
+++ precache-manifest.adacf6c576e6ba024124ad8841c6d429.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9766c79f124758ed81e",
+    "url": "/css/app.29464375.css"
+  },
+  {
+    "revision": "e0ea72a14d8fbc4ddf17",
+    "url": "/css/chunk-vendors.b9041969.css"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "28c35d5f79126e7f8411e3f190cbfba4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9766c79f124758ed81e",
+    "url": "/js/app.e5049157.js"
+  },
+  {
+    "revision": "e0ea72a14d8fbc4ddf17",
+    "url": "/js/chunk-vendors.bcc2fe91.js"
+  },
+  {
+    "revision": "76cb3fcf72128dc8d25bafafacbed440",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
